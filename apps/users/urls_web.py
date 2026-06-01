58aec19a6008e0c4387e70495fa3502ba6252f4f
Rafael Magnus de Lima Cerutti <111919049+RafaelCerutti@users.noreply.github.com>
2026-06-01 16:20:24 -0300

    adicao do template de clientes

--- apps/users/urls_web.py
+++ apps/users/urls_web.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
 from .views import (
+    ClienteListView,
+    ClienteDetailView,
+    ClienteCreateView,
+    ClienteUpdateView,
+    ClienteDeleteView,
     FuncionarioListView,
     FuncionarioDetailView,
     FuncionarioCreateView,
@@ -13,6 +18,14 @@ app_name = 'users_web'
 
 
 urlpatterns = [
+    path('clientes/', ClienteListView.as_view(), name='clientes_lista'),
+    path('clientes/listar/', ClienteListView.as_view(), name='clientes_listar'),
+    path('clientes/novo/', ClienteCreateView.as_view(), name='clientes_novo'),
+    path('clientes/criar/', ClienteCreateView.as_view(), name='clientes_criar'),
+    path('clientes/<int:pk>/', ClienteDetailView.as_view(), name='clientes_detalhe'),
+    path('clientes/<int:pk>/editar/', ClienteUpdateView.as_view(), name='clientes_editar'),
+    path('clientes/<int:pk>/excluir/', ClienteDeleteView.as_view(), name='clientes_excluir'),
+    path('clientes/<int:pk>/deletar/', ClienteDeleteView.as_view(), name='clientes_deletar'),
 
     path('funcionarios/', FuncionarioListView.as_view(), name='funcionarios_lista'),
     path('funcionarios/listar/', FuncionarioListView.as_view(), name='funcionarios_listar'),
